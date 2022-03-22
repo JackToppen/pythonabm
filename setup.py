538fdb40d9e806f4b206554215bacaa969a32069
JackToppen <jtoppen3@gatech.ed>
2022-03-21 22:08:06 -0500

    Updated version and kept markdown README

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="pythonabm",
-    version="0.2.0",
+    version="0.2.1",
     author="Jack Toppen",
     author_email="jtoppen3@gatech.edu",
     description="Framework for building Agent-Based Models (ABMs) in Python",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=["pythonabm"],
     python_requires=">=3.7",
     install_requires=[
